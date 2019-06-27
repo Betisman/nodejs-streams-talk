e3183fa0fa8104f56d697ca326bfc8844be87e38
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2019-06-27 02:32:48 +0200

    Remove logging all data and show counts instead for long example

--- 012_readableStream.js
+++ 012_readableStream.js
@@ -5,10 +5,12 @@ let chunks = 0;
 
 readableStream.on('data', function (chunk) {
   chunks++;
+  console.log(`chunk #${chunks}`);
+  if (chunks % 100 === 0) console.log(`chunk ${chunks}: ${chunk}`);
   data += chunk;
 });
 
 readableStream.on('end', function () {
-  console.log(data);
+  // console.log(data);
   console.log(`${chunks} chunks`)
 });
